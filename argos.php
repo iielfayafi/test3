115ca0b85deb8ac5f45e1ceebf005048ba10bad8
iielfayafi <43223061+iielfayafi@users.noreply.github.com>
2018-09-22 23:55:06 +0100

    Update and rename argos.html to argos.php

---
+++ argos.php
@@ -0,0 +1,35 @@
+<html>
+ <head>
+  <!-- Facebook Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '677226715783433');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=677226715783433&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+
+ </head><body><center>
+ <h1/>Your_chance_to_win_a_£500_voucher_for_Argos.</h1>
+<center >
+<a href="https://kind-bardeen-1a9c79.netlify.com/argore.html">
+<img src="https://user-images.githubusercontent.com/43223061/45599060-1f5e8800-b9dd-11e8-8610-360a9d9f1733.png" /></a></br>
+ </center><br/><br/><br/><p style="color:white;">
+<?
+$seed = floor(time()/(60*5));
+srand($seed);
+$item = rand(0,9);
+echo $item;
+?>
+</center>
+</p></body>
+</html>
